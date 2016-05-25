85132d2d87527a0d446dfdddac10164b7c456f81
Mark Penovich <mpenovich1@gmail.com>
2016-05-25 02:03:35 -0500

    my first commit

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,18 @@
+class TwilioController < ApplicationController
+  def send_sms
+    message = params[:message]
+    number = params[:number]
+    account_sid = 'AC44f3b3867b82ebb365858dd213b0e9c9'
+    auth_token = '9fa8a07fc6347b01b01c206ed43f80ab'
+
+
+
+    @client = Text::REST::Client.new account_sid, auth_token
+
+    @message = @client.account.messages.create({:to => "+1"+"#{number}",
+                                      :from => "c",
+                                      :body => "#{message}"})
+
+      redirect_to '/'
+  end
+end
